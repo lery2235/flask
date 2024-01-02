f8ecc3d232ee87ede1f378add507e2f4864de472
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-01-02 17:32:26 +0900

    Add files via upload

--- projects/myproject/pybo/models.py
+++ projects/myproject/pybo/models.py
@@ -1,10 +1,20 @@
 from pybo import db
 
+question_voter = db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)#글자수가 제한됬을 때 db.String 사용
     content = db.Column(db.Text(), nullable = False)#글자 수를 제한할 수 없는 텍스트 db.Text
     create_date = db.Column(db.DateTime(), nullable=False)# nullable 빈값 허용 여부
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
+    voter = db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -14,4 +24,17 @@ class Answer(db.Model):
     #backref는 여러 개의 답변이 달릴 수 있는데 역참조는 이 답변들을 참조할 수 있음.
     #파이썬 코드로 질문 데이터를 삭제할 때 연결된 답변 모두를 삭제하기 바란다면 다음처럼 db.backref 설정에 cascade='all, delete-orphan'를 추가해야 한다.
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
+    user = db.relationship('User', backref=db.backref('answer_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+
+
 
